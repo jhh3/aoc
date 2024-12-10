5617cb5ee280c23cdb1198c0110871e7c57a197d
John Holliman <github@jhh3.com>
2024-12-10 05:18:36 +0000

    feat: ✅ 2024/03 Part 2

--- 2024/day03/main.go
+++ 2024/day03/main.go
@@ -58,6 +58,65 @@ func (s *solver) SolvePart1(input string) string {
 }
 
 func (s *solver) SolvePart2(input string) string {
-	// todo
-	return ""
+
+	mulRe := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	// do() regex
+	doRe := regexp.MustCompile(`do\(\)`)
+	// don't() regex
+	dontRe := regexp.MustCompile(`don't\(\)`)
+
+	doMatches := doRe.FindAllStringIndex(input, -1)
+	dontMatches := dontRe.FindAllStringIndex(input, -1)
+
+	sum := 0
+	do := true
+	doIndex := 0
+	dontIndex := 0
+	start := 0
+
+	for doIndex < len(doMatches) && dontIndex < len(dontMatches) {
+		if do {
+			// find the next dont (past start)
+			dontInputIndex := dontMatches[dontIndex][1]
+			if dontInputIndex < start {
+				dontIndex++
+				continue
+			} else {
+				inputSubstr := input[start:dontInputIndex]
+				mulMatches := mulRe.FindAllStringSubmatch(inputSubstr, -1)
+				for _, match := range mulMatches {
+					// multiply
+					x, _ := strconv.Atoi(match[1])
+					y, _ := strconv.Atoi(match[2])
+					sum += x * y
+				}
+				start = dontInputIndex
+				do = false
+			}
+		} else {
+			// find the next do (past start)
+			doInputIndex := doMatches[doIndex][1]
+			if doInputIndex < start {
+				doIndex++
+				continue
+			} else {
+				start = doInputIndex
+				do = true
+			}
+		}
+	}
+
+	if do {
+		// do to end
+		inputSubstr := input[start:]
+		mulMatches := mulRe.FindAllStringSubmatch(inputSubstr, -1)
+		for _, match := range mulMatches {
+			// multiply
+			x, _ := strconv.Atoi(match[1])
+			y, _ := strconv.Atoi(match[2])
+			sum += x * y
+		}
+	}
+
+	return strconv.Itoa(sum)
 }
--- 2024/day03/main_test.go
+++ 2024/day03/main_test.go
@@ -10,6 +10,9 @@ import (
 //go:embed example_input.txt
 var exampleInput string
 
+//go:embed example_input_part2.txt
+var exampleInputPart2 string
+
 func Test_y2024d03(t *testing.T) {
 	common.RunTests(
 		&solver{},
@@ -23,9 +26,9 @@ func Test_y2024d03(t *testing.T) {
 			},
 			{
 				Name:  "provided-example",
-				Input: exampleInput,
+				Input: exampleInputPart2,
 				Part:  2,
-				Want:  "",
+				Want:  "48",
 			},
 		},
 	)
